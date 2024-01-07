ab56af22b2199bf5ea56dff08b8a726f0babf46d
Wendel <wendel4019@gmail.com>
2024-01-06 23:52:48 -0300

    Calculo de Média

---
+++ Excjava16(MEDIA)/src/CalculadoraMedia.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Calculadora de Média");
+        System.out.print("Quantos números você deseja inserir? ");
+        
+        int quantidadeNumeros = scanner.nextInt();
+        double soma = 0;
+
+        for (int i = 1; i <= quantidadeNumeros; i++) {
+            System.out.print("Digite o número #" + i + ": ");
+            double numero = scanner.nextDouble();
+            soma += numero;
+        }
+
+        double media = soma / quantidadeNumeros;
+        System.out.println("A média dos números é: " + media);
+
+        scanner.close();
+    }
+}
+
+
